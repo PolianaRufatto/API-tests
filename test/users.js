b3a2c94bf487d7d3a7c516da8bacd7f8d3660c70
Poliana Rufatto <polianarufatto@gmail.com>
2022-08-19 13:39:01 +0200

    add more tests scenarios to users

--- test/users.js
+++ test/users.js
@@ -1,13 +1,31 @@
 import supertest from 'supertest';
 import { expect } from 'chai'; 
 
-const request = supertest('https://gorest.co.in/public/');
+const request = supertest('https://gorest.co.in/public/v2/');
+const TOKEN = 'b8796f3a2648445f93a6ab9cf14d9460ddad8a3e8a2efa42902309e8ce7034a9';
 
 describe('Users', () => {
-  it('GET /users', (done) => {
-    request.get('v2/users?access-token=b8796f3a2648445f93a6ab9cf14d9460ddad8a3e8a2efa42902309e8ce7034a9').end((err, res) => {
+  it('GET /users', () => {
+    return request.get(`users?access-token=${TOKEN}`).then((res) => {
       expect(res.body).to.not.be.empty;
-      done();
-    })
+    });
+  });
+
+  it('GET /users/id', () => {
+    return request.get(`users/3540?access-token=${TOKEN}`).then((res) => {
+      expect(res.body.id).to.be.eq(3540);
+    });
+  });
+
+  it('GET /users with query params', () => {
+    const url = `users/?access-token=${TOKEN}&page=5&gender=female&status=active`;
+
+    return request.get(url).then((res) => {
+      expect(res.body).to.not.be.empty;
+      res.body.forEach((data) => {
+        expect(data.gender).to.eq('female');
+        expect(data.status).to.eq('active');
+      });
+    });
   });
 })
